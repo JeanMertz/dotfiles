056a97ce89e8976af47ddf1ff51f61828f7912bd
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-04-27 16:37:45 +0200

    Add functions to log activity

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function log {
+  echo -e "\033[1;31m>> \033[0;34m$*\033[0m"
+}
+function error {
+  echo -e "\033[1;31m!! \033[1;31m$*\033[0m"
+}
+function ask {
+  echo -e "\033[1;32m?? \033[0;32m$*\033[0m"
+}
+function pause {
+   read -p "$*"
+}
+
+
+echo -e "\033[1;32mInitializing Jean-iMac...\033[0m"
