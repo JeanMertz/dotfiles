0d616f7bdbc6b55add22084d7991fad13c4191be
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-04-28 08:27:07 +0200

    Update chef config to include json_attribs

--- chef/config/solo.rb
+++ chef/config/solo.rb
@@ -1,13 +1,12 @@
-root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+root_path = 			File.expand_path(File.join(File.dirname(__FILE__), '..'))
  
-cookbook_path   File.join(root_path, 'cookbooks')
-role_path       File.join(root_path, 'roles')
+cookbook_path			File.join(root_path, 'cookbooks')
+role_path				File.join(root_path, 'roles')
+json_attribs			File.join(root_path, 'config/node.json')
  
-# Move all the state stuff from /var/chef. I wish there was a single config
-# variable for this!
-state_root_path = File.expand_path('~/.dotfiles/chef/state')
-file_cache_path  "#{state_root_path}/cache"
-checksum_path    "#{state_root_path}/checksums"
-sandbox_path     "#{state_root_path}/sandbox"
-file_backup_path "#{state_root_path}/backup"
-cache_options[:path] = file_cache_path
+state_root_path = 		File.expand_path('~/.dotfiles/chef/state')
+file_cache_path			"#{state_root_path}/cache"
+checksum_path			"#{state_root_path}/checksums"
+sandbox_path			"#{state_root_path}/sandbox"
+file_backup_path		"#{state_root_path}/backup"
+cache_options[:path] = 	file_cache_path
