1e40df9fd0efb958031e00ba7fc0d1508d6d7dc9
Jean <jean.mertz@nulvierdrie.nl>
2012-05-03 18:11:53 +0200

    Updat chef-solo to use /tmp/chef-solo for tmp files instead of Dropbox

--- chef/config/solo.rb
+++ chef/config/solo.rb
@@ -4,9 +4,14 @@ cookbook_path			File.join(root_path, 'cookbooks')
 role_path				File.join(root_path, 'roles')
 json_attribs			File.join(root_path, 'config/node.json')
  
-state_root_path = 		File.expand_path('~/.dotfiles/chef/state')
+state_root_path = 		File.expand_path('/tmp/chef-solo/state')
 file_cache_path			"#{state_root_path}/cache"
 checksum_path			"#{state_root_path}/checksums"
 sandbox_path			"#{state_root_path}/sandbox"
 file_backup_path		"#{state_root_path}/backup"
 cache_options[:path] = 	file_cache_path
+
+FileUtils.mkdir_p file_cache_path
+FileUtils.mkdir_p checksum_path
+FileUtils.mkdir_p sandbox_path
+FileUtils.mkdir_p file_backup_path
