7a1b95cbdd804b7ca3d9fbd49122ca26e5265bd2
Jean <jean.mertz@nulvierdrie.nl>
2012-05-03 18:12:43 +0200

    Install Dropbox through the dmg_package command

--- chef/cookbooks/dropbox/recipes/default.rb
+++ chef/cookbooks/dropbox/recipes/default.rb
@@ -1,32 +1,6 @@
-unless File.exists?('/Applications/Dropbox.app')
-	# download dropbox
-	bash 'download_dropbox' do
-		code 'curl -L -o Dropbox.dmg https://www.dropbox.com/download?plat=mac'
-		not_if { File.exists?('Dropbox.dmg') }
-	end
-
-	# mount volume
-	bash 'mount_volume' do
-		code 'hdiutil mount Dropbox.dmg'
-		not_if { File.exists?('/Volumes/Dropbox Installer') }
-	end
-
-	# install Dropbox (copy .app to /Applications)
-	bash 'install_application' do
-		code "cp -R '/Volumes/Dropbox Installer/Dropbox.app' '/Applications/Dropbox.app'"
-	end
-
-	# unmount volume
-	bash 'unmount_volume' do
-		code 'hdiutil unmount "/Volumes/Dropbox Installer"'
-		only_if { File.exists?('/Volumes/Dropbox Installer') }
-	end
-
-	# delete dropbox file
-	bash 'unmount_volume' do
-		code 'rm Dropbox.dmg'
-		only_if { File.exists?('Dropbox.dmg') }
-	end
+# Install Dropbox from the dmg package
+dmg_package 'Dropbox' do
+	source 'http://www.dropbox.com/download?plat=mac'
 end
 
 # open dropbox and wait for user to set up dropbox
