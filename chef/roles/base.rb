33ea9151b8759e28ccff8c03fd4f019b9ee5eaca
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-04 14:53:46 +0200

    Chef add default attributes home_path, homebrew_path and homebrew_regex
    The homebrew_regex is meant to retrieve the path of a homebrew
    `formula`.
    
    When doing:
    
    `%x[brew info postgresql][node['homebrew_regex']]`
    
    It returns:
    
    `/usr/local/Cellar/postgresql/9.1.3`
    
    Which can then be used to manipulate the formula (like copying `plist`
    files to the `/Library/LaunchAgents` folder.

--- chef/roles/base.rb
+++ chef/roles/base.rb
@@ -4,6 +4,9 @@ application_support_path = "#{dotfiles_path}/Application Support"
 
 name 'base'
 override_attributes(
+	home_path: ENV['HOME'],
+	homebrew_path: `brew --prefix`.strip!,
+	homebrew_regex: /^#{`brew --prefix`.strip!}\/Cellar(?:(?! \(\d+ files?, \d+[a-z]\)).)+/i,
 	dotfiles_path: dotfiles_path,
 	dropbox_path: dropbox_path,
 	application_support_path: application_support_path
