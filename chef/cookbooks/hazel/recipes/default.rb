f2961ed1b8b616233f2cc5d770c999e68a672bd6
Jean <jean.mertz@nulvierdrie.nl>
2012-05-04 11:11:23 +0200

    Symlink Hazel license if present

--- chef/cookbooks/hazel/recipes/default.rb
+++ chef/cookbooks/hazel/recipes/default.rb
@@ -3,3 +3,10 @@ dmg_package 'Hazel' do
 	source 'hazel.dmg'
 	extension 'prefPane'
 end
+
+# symlink license file
+bash 'symlink_license' do
+	code 'mkdir -p "#{application_support_path}/Hazel"'
+	code %(ln -s "#{application_support_path}/Hazel/license" "#{ENV['HOME']}/Library/Application Support/Hazel")
+	only_if { File.exists?("#{application_support_path}/Hazel/license") && !File.exists?("#{ENV['HOME']}/Library/Application Support/Hazel") }
+end
