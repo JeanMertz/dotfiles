dba4765a6c1aeb747691bcc6c35aa83a89244163
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-05 14:06:31 +0200

    Dnsmasq Cookbook now also sets up OpenDNS

--- chef/cookbooks/dnsmasq/recipes/default.rb
+++ chef/cookbooks/dnsmasq/recipes/default.rb
@@ -4,10 +4,44 @@ formula = 'dnsmasq'
 # Install package
 package(formula)
 
+# Setup resolv
+execute "#{formula} Config" do
+	command %(echo "
+# Default:
+# 212.54.40.25
+# 212.54.35.25
+#
+# OpenDNS IPv6:
+nameserver 2620:0:ccc::2
+nameserver 2620:0:ccd::2
+#
+# OpenDNS:
+nameserver 208.67.222.222
+nameserver 208.67.220.220
+#
+# Google:
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+#
+# Google IPv6:
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844" > /usr/local/etc/resolv.dnsmasq.conf)
+	creates '/usr/local/etc/resolv.dnsmasq.conf'
+end
+
 # Setup config
 execute "#{formula} Config" do
-	command %(echo "domain-needed\naddress=/*.test/127.0.0.1" > /usr/local/etc/dnsmasq.conf)
+	command %(echo "
+domain-needed
+resolv-file=/usr/local/etc/resolv.dnsmasq.conf
+address=/*.test/127.0.0.1
+listen-address=127.0.0.1" > /usr/local/etc/dnsmasq.conf)
 	creates '/usr/local/etc/dnsmasq.conf'
+end
+
+# Make sure OS X only uses dnsmasq nameservers
+execute "echo 'nameserver 127.0.0.1' | sudo tee /etc/resolv.conf" do
+	not_if { %x[cat /etc/resolv.conf].match(/^(domain localdomain|nameserver 127.0.0.1)$/) }
 end
 
 # Launch on startup
