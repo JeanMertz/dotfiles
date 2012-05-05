e3ccb19e9597c5ab21b0555c045ad43148f72e82
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-05 12:40:29 +0200

    Fix zsh Cookbook switching shell automatically

--- chef/cookbooks/zsh/recipes/default.rb
+++ chef/cookbooks/zsh/recipes/default.rb
@@ -6,8 +6,14 @@ package(formula) do
 	options '--disable-etcdir'
 end
 
+# Add zsh as login shell
+execute "#{formula}_login_shell" do
+	command "echo #{node['homebrew_path']}/bin/zsh | sudo tee -a /etc/shells"
+	only_if { %x[cat /etc/shells | grep ^#{node['homebrew_path']}/bin/zsh$].empty? }
+end
+
 # Set default shell
 execute "#{formula}_default_shell" do
-	code "chsh -s #{node['homebrew_path']}/bin/zsh"
-	not_if { %x[echo $SHELL] == "#{node['homebrew_path']}/bin/zsh" }
+	command "chsh -s #{node['homebrew_path']}/bin/zsh"
+	not_if { %x[echo $SHELL].strip == "#{node['homebrew_path']}/bin/zsh" }
 end
