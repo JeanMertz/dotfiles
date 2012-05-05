12094774dca2f27be2ea112b46250842c7402766
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-05 11:38:36 +0200

    Switch to zsh shell after installing it

--- chef/cookbooks/zsh/recipes/default.rb
+++ chef/cookbooks/zsh/recipes/default.rb
@@ -5,3 +5,9 @@ formula = 'zsh'
 package(formula) do
 	options '--disable-etcdir'
 end
+
+# Set default shell
+execute "#{formula}_default_shell" do
+	code "chsh -s #{node['homebrew_path']}/bin/zsh"
+	not_if { %x[echo $SHELL] == "#{node['homebrew_path']}/bin/zsh" }
+end
