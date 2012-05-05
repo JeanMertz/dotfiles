b6715a2ddd7bd1fbdb4a823cd04e7a2fe3592538
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-05 17:21:04 +0200

    Run Alfred after installation

--- chef/cookbooks/alfred/recipes/default.rb
+++ chef/cookbooks/alfred/recipes/default.rb
@@ -5,3 +5,8 @@ formula = 'alfred'
 dmg_package(formula) do
 	source 'http://rwc.cachefly.net/alfred_1.2_220.dmg'
 end
+
+# Start application
+execute '/Applications/alfred.app &' do
+	only_if { %x[ps ax | grep "#{formula}" | grep -v "grep"].empty? }
+end
