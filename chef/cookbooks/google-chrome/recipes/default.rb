5d3e4d190affc437fe353371f093806f604c8318
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-05 12:38:03 +0200

    Fix Google Chrome Cookbook package to dmg_package

--- chef/cookbooks/google-chrome/recipes/default.rb
+++ chef/cookbooks/google-chrome/recipes/default.rb
@@ -2,7 +2,7 @@
 formula = 'Google Chrome'
 
 # Install package
-package(formula) do
+dmg_package(formula) do
 	dmg_name 'GoogleChrome'
 	source 'http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg'
 end
