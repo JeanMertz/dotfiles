8805f9528cc7a18ce75fdb397cd2135e873ac53a
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-06 17:11:51 +0200

    Add Caffeine Cookbook

---
+++ chef/cookbooks/caffeine/recipes/default.rb
@@ -0,0 +1,7 @@
+# Set formula
+formula = 'Caffeine'
+
+# Install package
+zip_app_package(formula) do
+	source 'http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine/Caffeine1.1.1.zip'
+end
