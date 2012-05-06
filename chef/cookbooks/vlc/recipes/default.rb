c1d851d5da4e0c1698736e8482dd070acffbbedf
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-06 16:57:32 +0200

    Add VLC Cookbook

---
+++ chef/cookbooks/vlc/recipes/default.rb
@@ -0,0 +1,8 @@
+# Set formula
+formula = 'VLC'
+
+# Install package
+dmg_package(formula) do
+	dmg_name 'vlc-2.0.1'
+	source 'http://dfn.dl.sourceforge.net/project/vlc/2.0.1/macosx/vlc-2.0.1.dmg'
+end
