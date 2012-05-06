9fcf616b90dc37c01be2f5f988f7946ce1d53042
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-06 17:03:30 +0200

    Add Perian Cookbook

---
+++ chef/cookbooks/perian/recipes/default.rb
@@ -0,0 +1,9 @@
+# Set formula
+formula = 'Perian'
+
+# Install package
+dmg_package(formula) do
+	dmg_name 'Perian_1.2.3'
+	source 'http://perian.cachefly.net/Perian_1.2.3.dmg'
+	extension 'prefPane'
+end
