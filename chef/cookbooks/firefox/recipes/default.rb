6cd6e9e05f2945a1624311582b41f8182c8737d4
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-07 14:17:18 +0200

    Update Firefox Cookbook to use different mirror

--- chef/cookbooks/firefox/recipes/default.rb
+++ chef/cookbooks/firefox/recipes/default.rb
@@ -3,5 +3,5 @@ formula = 'firefox'
 
 # Install package
 dmg_package(formula) do
-	source 'http://dm-download02.mozilla.org/pub/mozilla.org/firefox/releases/13.0b1/mac/en-US/Firefox%2013.0b1.dmg'
+	source 'http://pv-mirror01.mozilla.org/pub/mozilla.org/firefox/releases/13.0b2/mac/nl/Firefox%2013.0b2.dmg'
 end
