9cf039f7a6e9740a93028a1127ccee2509b27858
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-07 16:20:21 +0200

    Add Air Video Server Cookbook

---
+++ chef/cookbooks/air-video/recipes/default.rb
@@ -0,0 +1,12 @@
+# Set formula
+formula = 'Air Video Server'
+
+# Install package
+dmg_package(formula) do
+	source 'https://s3.amazonaws.com/AirVideo/Air+Video+Server+2.4.5-beta7u1.dmg'
+end
+
+# Start application
+execute "open /Applications/#{formula}.app &>/dev/null" do
+	only_if { %x[ps ax | grep "#{formula}" | grep -v "grep"].empty? }
+end
