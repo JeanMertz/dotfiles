b6ec0c4391155f787fc417e45f115f9a94d8b933
Jean Mertz <jean.mertz@nulvierdrie.nl>
2012-05-08 14:05:41 +0200

    Add Bartender Cookbook

---
+++ chef/cookbooks/bartender/recipes/default.rb
@@ -0,0 +1,12 @@
+# Set formula
+formula = 'Bartender'
+
+# Install package
+zip_app_package(formula) do
+	source 'ttp://www.macbartender.com/Demo/Bartender.zip'
+end
+
+# Start application
+execute "open /Applications/#{formula}.app &>/dev/null" do
+	only_if { %x[ps ax | grep "#{formula}" | grep -v "grep"].empty? }
+end
