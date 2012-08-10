fe4b18d359ec9febd6ea71bfdd70eda5dea32fb3
Jean Mertz <jean@mertz.fm>
2012-08-10 08:47:28 +0200

    Fix nodeJS remote url

--- chef/cookbooks/node/recipes/default.rb
+++ chef/cookbooks/node/recipes/default.rb
@@ -6,6 +6,6 @@ package(formula)
 
 # Install dependencies
 execute "#{formula} Dependencies" do
-	command 'curl http://npmjs.org/install.sh | sudo sh'
+	command 'curl https://npmjs.org/install.sh | sudo sh'
 	creates "#{node['homebrew_path']}/bin/npm"
 end
