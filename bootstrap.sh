d9a70777eb9dd29855be6a71f3dd953089b0a4f0
Jean Mertz <jean@mertz.fm>
2012-08-10 08:52:50 +0200

    Ask for administrator password upfront and keep it alive

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,12 @@ DROPBOX_DIR="$(bash ${BOOTSTRAP_DIR}/utilities/get_dropbox_folder.sh)"
 # Source all functions used throughout this script
 source "${BOOTSTRAP_DIR}/utilities/bootstrap_functions.sh"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 
 # Here we go!
 echo -e "\033[1;32mInitializing Mac configuration...\033[0m"
