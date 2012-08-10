a6892fb327dfddfe12f25952845b7fbe4adc8d6d
Jean Mertz <jean@mertz.fm>
2012-08-10 08:48:46 +0200

    Switch pow version from 0.4.0-pre to 0.4.0

--- chef/cookbooks/pow/recipes/default.rb
+++ chef/cookbooks/pow/recipes/default.rb
@@ -1,4 +1,4 @@
 # Can't install through homebrew, need HEAD
-execute 'curl get.pow.cx | VERSION=0.4.0-pre sh' do
+execute 'curl get.pow.cx | sh' do
 	creates '/Library/LaunchDaemons/cx.pow.firewall.plist'
 end
