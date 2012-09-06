e1c4244dea3097df3bd3d3cc784e42874ee91827
Jean Mertz <jean@mertz.fm>
2012-09-06 10:31:20 +0200

    Download Sublime Text 2 remotely

--- chef/cookbooks/sublime-text/recipes/default.rb
+++ chef/cookbooks/sublime-text/recipes/default.rb
@@ -1,42 +1,35 @@
 # Set formula
 formula = 'Sublime Text 2'
+version = '2218'
 
 # Install package
 dmg_package(formula) do
-	source "#{formula}.dmg"
+  source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%20#{version}.dmg"
 end
 
 # install command line tool
 execute "#{formula} Installation" do
-	command 'ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl'
-	creates '/usr/local/bin/subl'
-end
-
-# Unpack Application Support files if they don't exist
-dmg_package "#{formula} Application Support" do
-	app 'Sublime Text 2'
-	source 'Application Support.dmg'
-	extension false
-	destination node['application_support_path']
+  command 'ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl'
+  creates '/usr/local/bin/subl'
 end
 
 # symlink Application Support folder to dropbox
 execute "#{formula} Symlinks" do
-	command <<-EOH
-		mkdir -p "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
-		ln -s "#{node["application_support_path"]}/Sublime Text 2/Installed Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
-		ln -s "#{node["application_support_path"]}/Sublime Text 2/Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
-		ln -s "#{node["application_support_path"]}/Sublime Text 2/Pristine Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
-	EOH
-	creates "#{node["home_path"]}/Library/Application Support/Sublime Text 2"
+  command <<-EOH
+    mkdir -p "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
+    ln -s "#{node["application_support_path"]}/Sublime Text 2/Installed Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
+    ln -s "#{node["application_support_path"]}/Sublime Text 2/Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
+    ln -s "#{node["application_support_path"]}/Sublime Text 2/Pristine Packages" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/"
+  EOH
+  creates "#{node["home_path"]}/Library/Application Support/Sublime Text 2"
 end
 
 # symlink license file
 execute "#{formula} License" do
-	command <<-EOH
-		mkdir -p "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings"
-		ln -s "#{node["home_path"]}/Dropbox/dotfiles/Application Support/Sublime Text 2/Settings/License.sublime_license" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings"
-	EOH
-	creates "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings/License.sublime_license"
-	only_if { File.exists?("#{node["home_path"]}/Dropbox/dotfiles/Application Support/Sublime Text 2/Settings/License.sublime_license") }
+  command <<-EOH
+    mkdir -p "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings"
+    ln -s "#{node["home_path"]}/Dropbox/dotfiles/Application Support/Sublime Text 2/Settings/License.sublime_license" "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings"
+  EOH
+  creates "#{node["home_path"]}/Library/Application Support/Sublime Text 2/Settings/License.sublime_license"
+  only_if { File.exists?("#{node["home_path"]}/Dropbox/dotfiles/Application Support/Sublime Text 2/Settings/License.sublime_license") }
 end
