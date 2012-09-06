696270a28dd62d88eb25750a720f6eb7c0a20737
Jean Mertz <jean@mertz.fm>
2012-09-06 14:59:55 +0200

    Explain about possible missing SSD

--- bootstrap.sh
+++ bootstrap.sh
@@ -222,7 +222,7 @@ if [ "$(system_profiler SPSerialATADataType | grep 'TRIM Support: No' 2>/dev/nul
 	read confirm_trim
 	activate_trim_support
 else
-	log "TRIM support already enabled, to disable perfom the following actions:"
+	log "No SSD found or TRIM support already enabled, to disable perfom the following actions:"
 	log "  sudo perl -pi -e 's|(\x52\x6F\x74\x61\x74\x69\x6F\x6E\x61\x6C\x00).{9}(\x00\x51)|$1\x41\x50\x50\x4C\x45\x20\x53\x53\x44$2|sg' /System/Library/Extensions/IOAHCIFamily.kext/Contents/PlugIns/IOAHCIBlockStorage.kext/Contents/MacOS/IOAHCIBlockStorage"
 	log "  sudo kextcache -system-prelinked-kernel"
 	log "  sudo kextcache -system-caches"
