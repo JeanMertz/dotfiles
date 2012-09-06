fffde21c3dc29a4d57a8a2eae730f431ef354303
Jean Mertz <jean@mertz.fm>
2012-09-06 10:24:50 +0200

    Fix Tuxera NTFS dmg_name

--- chef/cookbooks/tuxera-NTFS/recipes/default.rb
+++ chef/cookbooks/tuxera-NTFS/recipes/default.rb
@@ -2,6 +2,8 @@
 version = '2012.3.3'
 
 dmg_package 'Tuxera_NTFS' do
+  app 'Install Tuxera NTFS'
+  dmg_name "Tuxera NTFS #{version}"
   source "http://www.tuxera.com/mac/tuxerantfs_#{version}.dmg"
   execute true
   installed_resource "/Library/PreferencePanes/Tuxera NTFS.prefPane"
