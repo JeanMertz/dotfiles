7fd7e5fa21f0c1f8ce33526cc3947f6e7ca668b9
Jean Mertz <jean@mertz.fm>
2012-09-06 10:24:12 +0200

    Change TotalFinder mpkg installer to pkg

--- chef/cookbooks/totalfinder/recipes/default.rb
+++ chef/cookbooks/totalfinder/recipes/default.rb
@@ -3,7 +3,7 @@ version = '1.4'
 
 dmg_package 'TotalFinder' do
   source "http://downloads-1.binaryage.com/TotalFinder-#{version}.dmg"
-	extension 'mpkg'
+	extension 'pkg'
 
 	not_if { File.exist?('/Applications/TotalFinder.app') }
 end
