b310cd7ac0342a36d3b3a907f571fd9eaa5dadaa
Jean Mertz <jean@mertz.fm>
2012-09-06 10:32:39 +0200

    Remove manual nom installation, done by homebrew now

--- chef/cookbooks/node/recipes/default.rb
+++ chef/cookbooks/node/recipes/default.rb
@@ -3,9 +3,3 @@ formula = 'node'
 
 # Install package
 package(formula)
-
-# Install dependencies
-execute "#{formula} Dependencies" do
-	command 'curl https://npmjs.org/install.sh | sudo sh'
-	creates "#{node['homebrew_path']}/bin/npm"
-end
