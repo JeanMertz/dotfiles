d71bf0ca6d7aad397b3888aa8613a4fb9a5d3b6f
Jean Mertz <jean@mertz.fm>
2012-09-06 10:31:51 +0200

    Add not_if statement to Hosts.prefPane installer

--- chef/cookbooks/prefpane-hosts/recipes/default.rb
+++ chef/cookbooks/prefpane-hosts/recipes/default.rb
@@ -2,7 +2,11 @@
 version = '1.2'
 file = "Hosts-#{version}.pkg"
 
-execute 'Install prefPane' do
-  %x[ wget -P "#{Chef::Config[:file_cache_path]}" https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-#{version}.pkg ]
-  %x[ sudo installer -pkg "#{Chef::Config[:file_cache_path]}/#{file}" -target / ]
+ruby_block 'hosts prefPane' do
+  block do
+    %x[ wget -P "#{Chef::Config[:file_cache_path]}" https://github.com/downloads/specialunderwear/Hosts.prefpane/Hosts-#{version}.pkg ]
+    %x[ sudo installer -pkg "#{Chef::Config[:file_cache_path]}/#{file}" -target / ]
+  end
+
+  not_if { File.exist?("#{node['root_prefpanes_path']}/Hosts.prefPane") }
 end
