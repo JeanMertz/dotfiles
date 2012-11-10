801fb15a7a732acfbc4b1b1235bed999077af0f4
Jean Mertz <jean@mertz.fm>
2012-11-10 09:20:08 +0100

    Add editors module to Puppet installation

---
+++ modules/editors/manifests/init.pp
@@ -0,0 +1,17 @@
+class editors {
+
+  # vim
+    # => https://github.com/adamhjk/adam-vim
+    # => http://gyaresu.org/hacking/2012/07/28/remap-space-bar-to-remove-search-highlighting-in-vim/
+    # => https://github.com/amix/vimrc
+    # => http://amix.dk/vim/vimrc.html
+    # => http://yanpritzker.com/2012/05/30/stop-using-colon-commands-in-vim/
+  # powerline
+    # => https://gist.github.com/1595572
+  # vim-rails
+  # sublime
+  # vundle
+    # => https://github.com/gmarik/vundle/issues/182
+    # => https://github.com/gmarik/vundle/wiki/Examples
+
+}
