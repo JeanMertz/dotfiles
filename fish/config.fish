b41e82593847e3e521b7fca3e9dd170cf8e8b875
Jean Mertz <jean@mertz.fm>
2019-09-23 22:37:47 +0200

    feat: colored man pages
    see:
    
    * https://unix.stackexchange.com/a/108840
    * https://unix.stackexchange.com/a/147

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,15 @@ set -gx XDG_CACHE_HOME  "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME   "$HOME/.local/share"
 
+# Colored man pages
+set -x LESS_TERMCAP_mb (printf "\e[01;31m")
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[01;32m")
+
 # Disable Homebrew Analytics
 set -gx HOMEBREW_NO_ANALYTICS 1
 
