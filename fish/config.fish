a15e692acf781a78608ff5fd28533e2f5701a8c3
Jean Mertz <git@jeanmertz.com>
2020-01-02 13:42:32 +0100

    feat: set XDG_RUNTIME_DIR variable

--- fish/config.fish
+++ fish/config.fish
@@ -73,6 +73,7 @@ set fish_pager_color_secondary      brgreen --background=magenta
 set -gx XDG_CACHE_HOME  "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME   "$HOME/.local/share"
+set -gx XDG_RUNTIME_DIR "$TMPDIR/.run"
 
 set -gx CARGO_HOME      "$XDG_CONFIG_HOME/rust/cargo"
 set -gx GNUPGHOME       "$XDG_CONFIG_HOME/gpg"
